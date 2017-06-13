6b7986b47030420aac574ea68233af6f6afe3f40
Dominic Vogl <dominic.vogl@sigikid.de>
2017-06-13 14:30:26 +0200

    combine the tasks to task for one run only --> better control and make the tasks multithread ready

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,12 @@ var changed = require('gulp-changed'),
     rename = require('gulp-rename'),
     imageResize = require('gulp-image-resize'),
     imagemin = require ('gulp-imagemin'),
-    plumber = require ('gulp-plumber')
-    watch = require('gulp-watch');
+    plumber = require ('gulp-plumber'),
+    watch = require('gulp-watch'),
+    parallel = require("concurrent-transform"),
+    os = require("os"),
+    gutil = require('gulp-util'),
+    logger = require('gulp-logger');
 
 // Pfade festlegen
 var imgSrc = [
@@ -19,14 +23,33 @@ var defaultTasks = [
     'watch'
 ];
 
+var resizeTasks = [
+    'thumb',
+    'large'
+];
+
+var resize = function(src, dest, options) {
+    gulp.src(src)
+        .pipe(plumber())
+        // .pipe(logger({
+        //     before: 'Starting Converting ...',
+        //     after: 'Converting complete!',
+        //     showChange: true
+        // }))
+        .pipe(parallel(
+            imageResize(options),
+            os.cpus().length
+        ))
+        .pipe(parallel(
+            imagemin(),
+            os.cpus().length
+        ))
+        // .pipe(rename({suffix: '-large'}))
+        .pipe(gulp.dest(dest))
+};
+
 gulp.task('default', defaultTasks);
-
-// Watch images files and resize and optimize
-gulp.task('images', function() {
-    console.log('\n---\n\n Watching Images, Optimizing Images \n\n---\n')
-    gulp.watch(imgSrc, ['thumb', 'imagemin']);
-});
-
+gulp.task('resize', resizeTasks);
 
 gulp.task('watch', function() {
     gulp.watch(imgSrc)
@@ -62,38 +85,24 @@ gulp.task('watch', function() {
         })
 });
 
-// Bilder und Vektoren minifizieren
-gulp.task('imagemin', function() {
-    gulp.src(imgSrc)
-        .pipe(changed(imgDst))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDst));
-});
+gulp.task('thumb', resize(imgSrc, imgDst + '250x250', {
+    width : 250,
+    height : 250,
+    crop : false,
+    upscale : false,
+    imageMagick: true,
+    quality: 0.75,
+    sharpen: true,
+    noProfile: true
+}));
 
-gulp.task('thumb', function () {
-    gulp.src(imgSrc)
-        .pipe(imageResize({
-            width : 250,
-            height : 250,
-            crop : false,
-            upscale : false,
-            imageMagick: true
-        }))
-        .pipe(imagemin())
-        // .pipe(rename({suffix: '-thumb'}))
-        .pipe(gulp.dest(imgDst+'/250x250/'));
-});
+gulp.task('large', resize(imgSrc, imgDst + '2000x2000', {
+    width : 2000,
+    height : 2000,
+    crop : false,
+    upscale : false,
+    imageMagick: true,
+    quality: 0.9,
+    noProfile: true
+}));
 
-gulp.task('large', function () {
-    gulp.src(imgSrc)
-        .pipe(imageResize({
-            width : 2000,
-            height : 2000,
-            crop : false,
-            upscale : false,
-            imageMagick: true
-        }))
-        .pipe(imagemin())
-        // .pipe(rename({suffix: '-large'}))
-        .pipe(gulp.dest(imgDst+'2000x2000'));
-});
