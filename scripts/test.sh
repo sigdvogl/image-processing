83331e97c207610ce64674be74b811a2cb18b227
Dominic Vogl <dominic.vogl@sigikid.de>
2017-06-16 11:08:46 +0200

    start new script with flexible parameters and logging

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,30 +7,19 @@ LOGFILE="/Users/vogl/Library/Logs/image_convert.log"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
 # Source and destination folders
-SRC="300dpi"
-DEST="2000x2000"
+BASEPATH="../images"
+SRC="${BASEPATH}/300dpi"
+DEST="${BASEPATH}/2000x2000"
 
 CopyFiles() {
     cp ${1}/$i ${2}/$3 2>> $LOGFILE
     echo "[$TIMESTAMP] copied file $3 from $1/ to $2/" >> $LOGFILE
-
-    #syslog -s -k Facility com.apple.console \
-    #             Level Error \
-    #             Sender MyScript \
-    #             Message "script says hello"
-
-}
+    }
 
 MoveFiles() {
     mv ${1}/$i ${2}/$3 2>> $LOGFILE
     echo "[$TIMESTAMP] moved file $3 from $1/ to $2/" >> $LOGFILE
-
-    #syslog -s -k Facility com.apple.console \
-    #             Level Error \
-    #             Sender MyScript \
-    #             Message "script says hello"
-
-}
+    }
 
 # start logging
 echo "[$TIMESTAMP] Start moving images..." >> $LOGFILE
@@ -41,10 +30,12 @@ mkdir -p "$DEST"
 # read file list and do
 cat list.txt | while read i; do
 
-# copy files from source to destination and log
+# copy files from src to dest and log
 if [ "$1" = "cp" ]
 then
     CopyFiles $SRC $DEST $i
+
+# move files from src to dest and log
 else
     MoveFiles $SRC $DEST $i
 fi
